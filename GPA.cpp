8f6c7f6e7801b42ce0ee6b127f132aa4466835a9
dromo7 <dromo7@horizon.csueastbay.edu>
2025-10-04 18:49:09 -0700

    Minimum

---
+++ GPA.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int amount;
+
+    cout << "Enter amount of courses: " << endl;
+    cin >> amount;
+
+    for (int i=0; i < amountl ++i) {
+        string course;
+        char grade;
+        int credits;
+        cout << "Enter course name: " << endl;  
+        cin >> course;
+        cout << "Enter grade received(A-F): " << endl;
+        cin >> grade;
+        cout << "Enter credits received: " << endl;
+        cin >> credits;
+
+        double points = 0.0;
+        int totalCredits = 0;
+        int totalPoints = 0;
+        if (grade == 'A') {
+            points = 4.0;
+        } else if (grade == 'B') {
+            points = 3.0;
+        } else if (grade == 'C') {
+            points = 2.0;
+        } else if (grade == 'D') {
+            points = 1.0;
+        } else if (grade == 'F') {
+            points = 0.0;
+        } else {
+            cout << "Invalid grade entered." << endl;
+            continue;
+        }
+
+        totalPoints += credits * points;
+        totalCredits += credits;
+
+
+
+    }
+}
